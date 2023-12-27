fb70458510a5aad663f058d1b20241e7fd5c8938
LuminousWanderer <1664524275@qq.com>
2023-12-27 19:30:07 +0800

    订单微服务

---
+++ Order/OrderService/src/test/java/com/retech/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retech.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
